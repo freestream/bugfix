dfd8d0fb087d409d27c6b50b74ec97903a4f925c
Anton Samuelsson <samuelsson.anton@gmail.com>
2020-04-16 09:36:46 +0000

    Updated to 1.9.4.4 deps

--- app/code/community/Mage/Adminhtml/Block/Widget/Form.php
+++ app/code/community/Mage/Adminhtml/Block/Widget/Form.php
@@ -20,7 +20,7 @@
  *
  * @category    Mage
  * @package     Mage_Adminhtml
- * @copyright  Copyright (c) 2006-2017 X.commerce, Inc. and affiliates (http://www.magento.com)
+ * @copyright  Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
@@ -204,13 +204,11 @@ class Mage_Adminhtml_Block_Widget_Form extends Mage_Adminhtml_Block_Widget
                     $element->setCanBeEmpty(true);
                 } else if ($inputType == 'date') {
                     $element->setImage($this->getSkinUrl('images/grid-cal.gif'));
-                    $element->setLocale(Mage::app()->getLocale()->getLocale());
                     $element->setFormat(Mage::app()->getLocale()->getDateFormatWithLongYear());
                 } else if ($inputType == 'datetime') {
                     $element->setImage($this->getSkinUrl('images/grid-cal.gif'));
                     $element->setTime(true);
                     $element->setStyle('width:50%;');
-                    $element->setLocale(Mage::app()->getLocale()->getLocale());
                     $element->setFormat(
                         Mage::app()->getLocale()->getDateTimeFormat(Mage_Core_Model_Locale::FORMAT_TYPE_SHORT)
                     );
@@ -256,3 +254,4 @@ class Mage_Adminhtml_Block_Widget_Form extends Mage_Adminhtml_Block_Widget
     }
 
 }
+
